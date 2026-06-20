f6a86103ed6850ce125388639b117bf2a8914d66
Oliver Petchey <olliesp07@gmail.com>
2026-06-20 18:59:07 +0100

    Created basic finite state controller

---
+++ scripts/finite state controller/FiniteStateController.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public partial class FiniteStateController : Node
+{
+	// If state is null then nothing will happen and the state can never be changed
+	private IState state;
+
+    // Override this to set the initial state
+    public override void _Ready()
+	{
+		base._Ready();
+	}
+
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+		if (state != null)
+		{
+			state.Update(delta);
+			IState nextState = state.NextState();
+			if (nextState != null && nextState.GetType() != this.state.GetType()) {
+				this.state.OnLeave();
+				nextState.OnEnter();
+			}
+			this.state = nextState;
+		}
+	}
+}
