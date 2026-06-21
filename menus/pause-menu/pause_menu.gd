357facaadf5f98ec3198735d978a432b213323a5
astraex5356 <leiagardiner2308@gmail.com>
2026-06-21 02:58:55 +0100

    pause menu and other buttons

---
+++ menus/pause-menu/pause_menu.gd
@@ -0,0 +1,39 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AnimationPlayer.play("RESET")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	testEsc()
+	
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+	
+func testEsc():
+	if Input.is_action_just_pressed("escape") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("escape") and get_tree().paused == true:
+		resume()
+
+
+func _on_resume_pressed() -> void:
+	resume()
+
+
+func _on_restart_pressed() -> void:
+	resume()
+	get_tree().reload_current_scene()
+
+
+func _on_exit_pressed() -> void:
+	resume()
+	get_tree().change_scene_to_file("res://main-menu/main-menu.tscn")
