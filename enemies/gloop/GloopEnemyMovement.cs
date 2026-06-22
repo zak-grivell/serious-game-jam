a3c15e2abd446bc30064df3511d8ce7b809d851e
Oliver Petchey <olliesp07@gmail.com>
2026-06-22 17:13:15 +0100

    Can now edit goomba-style movement paramaters - yipppeeee! ^v^

--- enemies/gloop/GloopEnemyMovement.cs
+++ enemies/gloop/GloopEnemyMovement.cs
@@ -3,13 +3,27 @@ using System;
 
 public partial class GloopEnemyMovement : FiniteStateController
 {
-	[Export] private float pos1, pos2, speed;
+    [Export] private Node root;
+    private float pos1, pos2, speed;
 	[Export] private CharacterBody2D rb;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		base._Ready();
-		this.state = SimpleGoombaMovement.ToFirst(pos1, pos2, speed, rb);
+		InitNewState(
+			(float) root.GetMeta("pos1"),
+			(float) root.GetMeta("pos2"),
+			(float) root.GetMeta("speed")
+        );
+	}
+
+	private void CreateState() => this.state = SimpleGoombaMovement.ToFirst(pos1, pos2, speed, rb);
+
+    public void InitNewState(float pos1, float pos2, float speed) {
+		this.pos1 = pos1;
+		this.pos2 = pos2;
+		this.speed = speed;
+		CreateState();
 	}
 }
