774e6143bf70807ee0f2d31cea67f4450214019a
zak grivell <zak@grivell.uk>
2026-06-26 12:50:59 +0100

    add on bosses loading in

--- scripts/finite state controller/FiniteStateController.cs
+++ scripts/finite state controller/FiniteStateController.cs
@@ -1,19 +1,23 @@
 using Godot;
 using System;
 
-public partial class FiniteStateController : Node
+public partial class FiniteStateController : Node2D
 {
 	// If state is null then nothing will happen and the state can never be changed
 	protected IState state;
 
+	private Node2D player;
+
 	// Override this to set the initial state
 	public override void _Ready()
 	{
+		player = (Node2D)GetTree().GetFirstNodeInGroup("Player");	
 		base._Ready();
 	}
 
 	public override void _Process(double delta)
 	{
+		if (player == null || GlobalPosition.DistanceTo(player.GlobalPosition) > 300) return;
 		base._Process(delta);
 		if (state != null)
 		{
